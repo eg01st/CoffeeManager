1bf6b05e1c202f7860dffeb553ee84ef03e8c5b7
dmytro.serdechnyi <dmytro.serdechnyi@trinetix.com>
2016-11-14 15:30:06 +0200

    orders

--- CoffeeManager.Core/CoffeeManager.Models/OrderItem.cs
+++ CoffeeManager.Core/CoffeeManager.Models/OrderItem.cs
@@ -2,12 +2,12 @@
 {
     public class OrderItem : Entity
     {
-        public bool IsDone { get;  set; }
+        public bool IsDone { get; set; }
 
         public int SuplyProductId { get; set; }
-
-        public int? OrderId { get;  set; }
-        public decimal Price { get;  set; }
-        public decimal Quantity { get;  set; }
+        public string SuplyProductName { get; set; }
+        public int? OrderId { get; set; }
+        public decimal Price { get; set; }
+        public decimal Quantity { get; set; }
     }
 }
