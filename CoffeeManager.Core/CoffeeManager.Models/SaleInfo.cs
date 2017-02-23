506d2cac93ae125e0ad2f56564ae425f6f6d3652
Dima <Dima@HOMESERVER>
2017-02-24 00:08:52 +0200

    credit card

---
+++ CoffeeManager.Core/CoffeeManager.Models/SaleInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeManager.Models
+{
+    public class SaleInfo
+    {
+        public string Name { get; set; }
+        public Nullable<decimal> Amount { get; set; }
+        public Nullable<int> Quantity { get; set; }
+        public Nullable<int> Producttype { get; set; }
+    }
+}
