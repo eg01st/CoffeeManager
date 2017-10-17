cf06406f9628c3873ff2838b41bcc47aeac4c040
dmytro.serdechnyi <dserdechny@deloitte.com>
2017-10-17 16:09:20 +0300

    refactoirng

--- CoffeeManager.Core/Admin/CoffeeManagerAdmin.iOS/Views/Abstract/MainTabController.cs
+++ CoffeeManager.Core/Admin/CoffeeManagerAdmin.iOS/Views/Abstract/MainTabController.cs
@@ -5,6 +5,7 @@ using UIKit;
 using CoffeeManagerAdmin.Core;
 using MvvmCross.Platform;
 using Foundation;
+using System.Collections.Generic;
 
 namespace CoffeeManagerAdmin.iOS
 {
@@ -69,82 +70,29 @@ namespace CoffeeManagerAdmin.iOS
 
             base.ViewDidLoad();
 
-            var moneyViewModel = ProduceViewModel(typeof(MoneyViewModel));
-            var moneyView = ProduceView(moneyViewModel);
+            var vcs = new List<UIViewController>();
 
-            var selectedMoneyImage = UIImage.FromBundle("ic_attach_money.png");
-            selectedMoneyImage = selectedMoneyImage.ImageWithRenderingMode(UIImageRenderingMode.AlwaysOriginal);
+            foreach (var item in new []
+            { 
+                new { Type = typeof(MoneyViewModel), Icon = "ic_attach_money.png", Title = "Финансы" },
+                new { Type = typeof(StorageViewModel), Icon = "ic_shopping_basket.png", Title = "Склад" },
+                new { Type = typeof(ManageExpensesViewModel), Icon = "ic_arrow_upward.png", Title = "Расходы" },
+                new { Type = typeof(ProductsViewModel), Icon = "ic_local_cafe.png", Title = "Товары" },
+                new { Type = typeof(StatisticViewModel), Icon = "ic_trending_up.png", Title = "Статистика" },
+            })
+            {
+                var vm = ProduceViewModel(item.Type);
+                var view = ProduceView(vm);
 
-            var moneyImage = UIImage.FromBundle("ic_attach_money_white.png");
-            moneyImage = moneyImage.ImageWithRenderingMode(UIImageRenderingMode.AlwaysOriginal);
+                var icon = UIImage.FromBundle(item.Icon);
+                icon = icon.ImageWithRenderingMode(UIImageRenderingMode.AlwaysTemplate);
 
-            var moneytabBarItem = new UITabBarItem("Финансы", moneyImage, selectedMoneyImage);
+                var tabBarItem = new UITabBarItem(item.Title, icon, icon);
+                view.TabBarItem = tabBarItem;
+                vcs.Add(view);
+            }
 
-
-
-            var storageViewModel = ProduceViewModel(typeof(StorageViewModel));
-            var storageView = ProduceView(storageViewModel);
-
-            var selectedStorageImage = UIImage.FromBundle("ic_shopping_basket.png");
-            selectedStorageImage = selectedStorageImage.ImageWithRenderingMode(UIImageRenderingMode.AlwaysOriginal);
-
-            var storageImage = UIImage.FromBundle("ic_shopping_basket_white.png");
-            storageImage = storageImage.ImageWithRenderingMode(UIImageRenderingMode.AlwaysOriginal);
-
-            var storagetabBarItem = new UITabBarItem("Склад", storageImage, selectedStorageImage);
-
-
-            var expensesViewModel = ProduceViewModel(typeof(ManageExpensesViewModel));
-            var expensesView = ProduceView(expensesViewModel);
-
-            var selectedExpensesImage = UIImage.FromBundle("ic_arrow_upward.png");
-            selectedExpensesImage = selectedExpensesImage.ImageWithRenderingMode(UIImageRenderingMode.AlwaysOriginal);
-
-            var expensesImage = UIImage.FromBundle("ic_arrow_upward_white.png");
-            expensesImage = expensesImage.ImageWithRenderingMode(UIImageRenderingMode.AlwaysOriginal);
-
-            var expensesTabBarItem = new UITabBarItem("Расходы", expensesImage, selectedExpensesImage);
-
-
-
-            var productsViewModel = ProduceViewModel(typeof(ProductsViewModel));
-            var productsView = ProduceView(productsViewModel);
-
-            var selectedProductsImage = UIImage.FromBundle("ic_local_cafe.png");
-            selectedProductsImage = selectedProductsImage.ImageWithRenderingMode(UIImageRenderingMode.AlwaysOriginal);
-
-            var productsImage = UIImage.FromBundle("ic_local_cafe_white.png");
-            productsImage = productsImage.ImageWithRenderingMode(UIImageRenderingMode.AlwaysOriginal);
-
-            var productsTabBarItem = new UITabBarItem("Товары", productsImage, selectedProductsImage);
-
-        
-
-
-            var statisticViewModel = ProduceViewModel(typeof(StatisticViewModel));
-            var statisticView = ProduceView(statisticViewModel);
-
-            var selectedstatisticImage = UIImage.FromBundle("ic_trending_up.png");
-            selectedstatisticImage = selectedstatisticImage.ImageWithRenderingMode(UIImageRenderingMode.AlwaysOriginal);
-
-            var statisticImage = UIImage.FromBundle("ic_trending_up_white.png");
-            statisticImage = statisticImage.ImageWithRenderingMode(UIImageRenderingMode.AlwaysOriginal);
-
-            var statistictabBarItem = new UITabBarItem("Статистика", statisticImage, selectedstatisticImage);
-
-
-            moneyView.TabBarItem = moneytabBarItem;
-
-            storageView.TabBarItem = storagetabBarItem;
-
-            productsView.TabBarItem = productsTabBarItem;
-            statisticView.TabBarItem = statistictabBarItem;
-            expensesView.TabBarItem = expensesTabBarItem;
-
-            var controllers = new[] {moneyView, storageView, productsView, expensesView, statisticView };
-
-
-            ViewControllers = controllers;
+            ViewControllers = vcs.ToArray();
 
             DoViewDidLoad();
         }
--- CoffeeManager.Core/Admin/CoffeeManagerAdmin.iOS/Views/MainView.cs
+++ CoffeeManager.Core/Admin/CoffeeManagerAdmin.iOS/Views/MainView.cs
@@ -1,13 +1,6 @@
-﻿using System;
+﻿
 using Foundation;
 using UIKit;
-using MvvmCross.iOS.Views;
-using MvvmCross.Binding.BindingContext;
-using CoffeeManagerAdmin.Core.ViewModels;
-using CoreGraphics;
-using MvvmCross.Binding.iOS.Views;
-using CoffeManager.Common;
-using System.Collections.Generic;
 using MvvmCross.iOS.Views.Presenters.Attributes;
 
 namespace CoffeeManagerAdmin.iOS
@@ -33,12 +26,8 @@ namespace CoffeeManagerAdmin.iOS
         private void InitTabBar()
         {
             var tabBar = TabBar;
-           // TabBar.BackgroundColor = UIColor.LightGray;
-          //  TabBar.TintColor = UIColor.White;
-
-          //  UITabBarItem.Appearance.SetTitleTextAttributes(new UITextAttributes() { TextColor = UIColor.White }, UIControlState.Normal);
-           // UITabBarItem.Appearance.SetTitleTextAttributes(new UITextAttributes() {  TextColor = UIColor.Black }, UIControlState.Selected);
-        
+            tabBar.UnselectedItemTintColor = UIColor.Black;
+            tabBar.SelectedImageTintColor = UIColor.FromRGB(0, 122, 255);
         }
 
     }
