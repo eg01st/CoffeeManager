73609a7ddee08b31647c825b59e6a3a2f41b2062
dmytro.serdechnyi <dmytro.serdechnyi@trinetix.com>
2017-11-07 17:19:40 +0200

    icons

--- CoffeeManager.Core/Client/CoffeeManager.Droid/SplashScreen.cs
+++ CoffeeManager.Core/Client/CoffeeManager.Droid/SplashScreen.cs
@@ -5,7 +5,7 @@ using MvvmCross.Droid.Views;
 
 namespace CoffeeManager.Droid
 {
-    [Activity(Label = "Coffee Manager", MainLauncher = true, Icon = "@drawable/icon", ScreenOrientation = ScreenOrientation.Portrait)]
+    [Activity(Label = "Coffee Manager", MainLauncher = true, Icon = "@mipmap/ic_launcher", ScreenOrientation = ScreenOrientation.Portrait)]
     public class SplashScreen : MvxSplashScreenActivity
     {
         public SplashScreen() : base(Resource.Layout.SplashScreen)
