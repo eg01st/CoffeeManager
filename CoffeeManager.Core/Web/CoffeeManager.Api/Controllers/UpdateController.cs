b6cc39e4a06291d2d3b75c147724899d91048166
Dima <Dima@HOMESERVER>
2018-02-22 11:48:19 +0200

    autoupdate

---
+++ CoffeeManager.Core/Web/CoffeeManager.Api/Controllers/UpdateController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using CoffeeManager.Api.Helpers;
+using CoffeeManager.Api.Mappers;
+
+namespace CoffeeManager.Api.Controllers
+{
+    public class UpdateController : ApiController
+    {
+        [Route("api/update/getcurrentversion")]
+        [HttpGet]
+        public async Task<HttpResponseMessage> GetCurrenVersion(HttpRequestMessage message)
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, 3);
+        }
+
+
+        [Route("api/update/GetAndroidPackage")]
+        [HttpGet]
+        public async Task<HttpResponseMessage> GetAndroidPackage(HttpRequestMessage message)
+        {
+            string root = HttpContext.Current.Server.MapPath("~/App_Data");
+            string fullName = root + "/android/CoffeeManager.Droid.apk";
+
+            if (!File.Exists(fullName))
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, "App does not exists");
+            }
+
+            var zip = new FileStreamEx(fullName);
+
+            var response = Request.CreateResponse();
+
+            response.Content = new PushStreamContent(zip.Write, new MediaTypeHeaderValue("file/zip"));
+            response.Content.Headers.ContentDisposition = new ContentDispositionHeaderValue("attachment")
+            {
+                FileName = "CoffeeManager.Droid-Signed.apk"
+            };
+            return response;
+        }
+    }
+}
