d724697ee7a56104fcdacbffe6be409f4de44f77
dmytro.serdechnyi <dmytro.serdechnyi@trinetix.com>
2018-04-09 02:24:52 +0300

    wip

--- CoffeeManager.Core/Client/CoffeeManager.Core/ViewModels/LoginViewModel.cs
+++ CoffeeManager.Core/Client/CoffeeManager.Core/ViewModels/LoginViewModel.cs
@@ -14,24 +14,35 @@ namespace CoffeeManager.Core.ViewModels
 {
     public class LoginViewModel : ViewModelBase
     {
-        private readonly IUserManager _userManager;
-        private readonly IShiftManager _shiftManager;
-        private UserDTO[] _users;
+        private readonly IUserManager userManager;
+        private readonly IShiftManager shiftManager;
+        private UserDTO[] users;
 
         public ICommand SelectUserCommand { get; }
         public ICommand RefreshCommand { get; }
 
-        readonly IAccountManager accountManager;
-        readonly ILocalStorage localStorage;
-
-        public LoginViewModel(IShiftManager shiftManager, IUserManager userManager, IAccountManager accountManager, ILocalStorage localStorage)
+        public UserDTO[] Users
         {
-            this.localStorage = localStorage;
-            this.accountManager = accountManager;
-            _userManager = userManager;
-            _shiftManager = shiftManager;
+            get => users;
+            set
+            {
+                users = value;
+                RaisePropertyChanged(nameof(Users));
+            }
+        }
+        
+        public LoginViewModel(IShiftManager shiftManager, IUserManager userManager)
+        {
+            this.userManager = userManager;
+            this.shiftManager = shiftManager;
             SelectUserCommand = new MvxCommand<UserDTO>(DoSelectUser);
             RefreshCommand = new MvxAsyncCommand(DoRefresh);
+        }
+        
+        public override async Task Initialize()
+        {
+            var res = await ExecuteSafe(userManager.GetUsers);
+            Users = res?.Where(u => u.IsActive).ToArray();
         }
 
         private async Task DoRefresh()
@@ -64,28 +75,9 @@ namespace CoffeeManager.Core.ViewModels
 
             await ExecuteSafe(async () =>
             {
-                int shiftId = await _shiftManager.StartUserShift(user.Id, counter.Value);
+                int shiftId = await shiftManager.StartUserShift(user.Id, counter.Value);
                 await NavigationService.Navigate<MainViewModel, Shift>(new Shift {  UserId = user.Id, Id = shiftId });
             });
         }
-
-        public UserDTO[] Users
-        {
-            get { return _users; }
-            set
-            {
-                _users = value;
-                RaisePropertyChanged(nameof(Users));
-            }
-        }
-
-        public async override Task Initialize()
-        {
-            var res = await ExecuteSafe(_userManager.GetUsers);
-            Users = res?.Where(u => u.IsActive).ToArray();
-        }
-
-       
-
     }
 }
